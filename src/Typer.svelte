1530c3ffd117af5f911eccfbcec0b491a87205cb
Dunks <duncan.nicoll@gmail.com>
2021-05-27 23:42:28 +0100

    working out accuracy

--- src/Typer.svelte
+++ src/Typer.svelte
@@ -1,5 +1,6 @@
 <script>
-  // let sentences = ["saw", "are", "different"];
+  import * as diff from "diff";
+  // let sentences = ["saw also go", "are would saw", "back two air"];
   let sentences = [
     "saw also go does over have left went below almost the which word him right here his out of away only only day add went up we take must never like are number should found",
     "are would saw word about own place which more will more those does behind give go one too look never said down here had small to so the had has your how different one every",
@@ -12,8 +13,14 @@
   let endTime = 0;
   let win = 0;
   let lose = 0;
+  let characterScore = 0;
   let finished = false;
   let totalWords = sentences.map(s => s.split(" ")).flat().length;
+  let totalCharacters = sentences
+    .map(s => s.split(" "))
+    .flat()
+    .join("").length;
+
   $: grossWPM = finished
     ? typedWords.join().length / 5 / (duration / minute)
     : 0;
@@ -28,56 +35,92 @@
   let buildId = process.env.BUILD_ID;
 
   let wordPointer = 0;
-  let currentValue = "";
+  let submittedText = "";
+  let typedCharacters = "";
   let minute = 60000;
+
+  const addScores = (acc, curr) => acc + curr;
+
+  const sumScoreByProp = (results, prop) =>
+    results
+      .filter(result => result[prop])
+      .map(item => item.count)
+      .reduce(addScores, 0);
+
+  const sumScoreWithoutAddedRemoved = results =>
+    results
+      .filter(result => !result.added && !result.removed)
+      .map(item => item.count)
+      .reduce(addScores, 0);
+
+  const checkAndScore = (allCharacters, submittedWord, correctWord) => {
+    const accuracyResults = diff.diffChars(correctWord, allCharacters.trim());
+    const submittedResults = diff.diffChars(correctWord, submittedWord.trim());
+
+    const accuracyAdded = sumScoreByProp(accuracyResults, "added");
+    const accuracyRemoved = sumScoreByProp(accuracyResults, "removed");
+    const typedScore = sumScoreWithoutAddedRemoved(accuracyResults);
+
+    const submittedAdded = sumScoreByProp(submittedResults, "added");
+    const submittedRemoved = sumScoreByProp(submittedResults, "removed");
+
+    const accuracyErrors = accuracyAdded + accuracyRemoved;
+    const submittedErrors = submittedAdded + submittedRemoved;
+
+    totalCharacters += accuracyErrors;
+    characterScore += typedScore;
+
+    if (!submittedErrors) {
+      words[wordPointer].result = 1;
+      win += 1;
+    } else {
+      words[wordPointer].result = -1;
+      lose += 1;
+    }
+  };
 
   let onKeyPress = e => {
     if (!startTime) {
       startTime = Date.now();
     }
+    if (e.code !== "Space" && e.code !== "Enter") {
+      typedCharacters += e.key;
+    }
     if (e.code === "Space" || e.code === "Enter") {
       e.preventDefault();
 
       if (wordPointer < words.length - 1) {
-        if (currentValue === words[wordPointer].word) {
-          words[wordPointer].result = 1;
-          win += 1;
-        } else {
-          words[wordPointer].result = -1;
-          lose += 1;
-        }
-        typedWords = [...typedWords, currentValue];
+        checkAndScore(typedCharacters, submittedText, words[wordPointer].word);
+        typedCharacters = "";
+        typedWords = [...typedWords, submittedText];
         wordPointer++;
       } else {
         if (sentencePointer < sentences.length - 1) {
-          if (currentValue === words[wordPointer].word) {
-            words[wordPointer].result = 1;
-            win += 1;
-          } else {
-            words[wordPointer].result = -1;
-            lose += 1;
-          }
-
-          typedWords = [...typedWords, currentValue];
+          checkAndScore(
+            typedCharacters,
+            submittedText,
+            words[wordPointer].word
+          );
+          typedCharacters = "";
+          typedWords = [...typedWords, submittedText];
           sentencePointer++;
           wordPointer = 0;
         } else {
           if (!finished) {
-            if (currentValue === words[wordPointer].word) {
-              words[wordPointer].result = 1;
-              win += 1;
-            } else {
-              words[wordPointer].result = -1;
-              lose += 1;
-            }
-            typedWords = [...typedWords, currentValue];
+            checkAndScore(
+              typedCharacters,
+              submittedText,
+              words[wordPointer].word
+            );
+            typedCharacters = "";
+            typedWords = [...typedWords, submittedText];
             endTime = Date.now();
             finished = true;
             console.log("finished");
           }
         }
       }
-      currentValue = "";
+      submittedText = "";
     }
   };
 </script>
@@ -92,6 +135,9 @@
     <h4>Minute: {minute}</h4>
     <h4>Duration: {duration}</h4>
     <h4>Won: {win}</h4>
+    <h4>sentencePointer: {sentencePointer}</h4>
+    <h4>wordPointer: {wordPointer}</h4>
+    <h4>word length: {words.length}</h4>
   {/if}
   {#if finished}
     <div class="mb-6 text-2xl w-6/12">
@@ -100,7 +146,7 @@
         {win}/{totalWords}
       </div>
       <div>
-        <span class="font-bold">Mistakes:</span>
+        <span class="font-bold">Errors:</span>
         {lose}
       </div>
       <div>
@@ -110,6 +156,10 @@
       <div>
         <span class="font-bold">Net WPM:</span>
         {Math.round(netWPM)}
+      </div>
+      <div>
+        <span class="font-bold">Accuracy:</span>
+        {Number.parseFloat((characterScore / totalCharacters) * 100).toFixed(2)}
       </div>
     </div>
   {/if}
@@ -132,7 +182,7 @@
     <input
       class="border-2 text-xl p-2"
       on:keypress={onKeyPress}
-      bind:value={currentValue} />
+      bind:value={submittedText} />
   {/if}
 
   <div class="text-gray-300 text-xs mt-6">build: {buildId}</div>
